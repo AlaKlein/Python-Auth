5c04eab8bc8da3e41bd8923e88bb09895db04f33
Alã Klein <alaeklein@gmail.com>
2025-08-26 01:13:58 -0300

    Added a logic to switch the ldap connection to LDAPS if the LDAP_PORT is 636.

--- ldap_auth.py
+++ ldap_auth.py
@@ -5,18 +5,24 @@ import ssl
 #Load config attributes
 config = configparser.ConfigParser()
 config.read("ldap_config")
+ldap_server = ""
 LDAP_SERVER_IP = config["ldap"]["ldap_server"]
 LDAP_PORT = config["ldap"]["ldap_port"]
-LDAP_SERVER = f"ldap://{LDAP_SERVER_IP}:{LDAP_PORT}"
 BASE_DN = config["ldap"]["base_dn"]
 BIND_DN = config["ldap"]["bind_dn"]
 BIND_PASSWORD = config["ldap"]["bind_password"]
 LDAP_ATTRIBUTE_MAPPING = config["ldap"]["ldap_attribute_mapping"]
 
 def connect_with_bind_user():
-    server = Server(LDAP_SERVER, get_info=ALL)
+    global ldap_server
+    if int(LDAP_PORT) == 389:
+        ldap_server = f"ldap://{LDAP_SERVER_IP}:{LDAP_PORT}"
+    elif int(LDAP_PORT) == 636:
+        ldap_server = f"ldaps://{LDAP_SERVER_IP}:{LDAP_PORT}"
+    print(ldap_server)
+    server = Server(ldap_server, get_info=ALL)
     conn = Connection(server, user=BIND_DN, password=BIND_PASSWORD, auto_bind=True)
-    print(f"Connected to {LDAP_SERVER}")
+    print(f"Connected to {ldap_server}")
     return conn
 
 def get_user_by_attribute_mapping(conn, user, attributes=None):
